88dbd6495a10acf08548c6353288915d4e17a815
Jaafer Mahfoud <jaafer.mafhoud97@gmail.com>
2024-11-04 21:41:49 +0300

    almost finished the server manager and getting all the information

---
+++ lib/cubit/news_state.dart
@@ -0,0 +1,17 @@
+import 'package:new_project/models/news_model.dart';
+
+abstract class NewsState {}
+
+class NewsInitialState extends NewsState {}
+
+class NewsLoadingState extends NewsState {}
+
+class NewsSuccessState extends NewsState {
+  NewsModel newsModel;
+  NewsSuccessState(this.newsModel);
+}
+
+class NewsErrorState extends NewsState {
+  String errorMessage;
+  NewsErrorState(this.errorMessage);
+}
--- lib/models/news_model.dart
+++ lib/models/news_model.dart
@@ -3,15 +3,28 @@ class NewsModel {
   String publishedAt;
   String author;
   String title;
-  String description;
+  String? description;
   String? imageUrl;
 
   NewsModel({
     required this.source,
     required this.author,
     required this.publishedAt,
-    required this.description,
+    this.description,
     required this.title,
     this.imageUrl,
   });
+
+  factory NewsModel.fromJson(Map<String, dynamic> data, int i) {
+    return NewsModel(
+      source: data['articles'][i]['source']['name'],
+      author: data['articles'][i]['author'],
+      publishedAt: data['articles'][i]['publishedAt'],
+      description:
+          data['articles'][i]['description'] ?? 'This is really EXCITING NEWS',
+      title: data['articles'][i]['title'],
+      imageUrl: data['articles'][i]['urlToImage'] ??
+          'https://media.gettyimages.com/id/1339466666/nl/vector/breaking-news.jpg?s=612x612&w=gi&k=20&c=zFdbjWQZxPDOk0PUKDUDV8roLM31yF0TwUh0R6x_svA=',
+    );
+  }
 }
